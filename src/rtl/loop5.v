ad29cf693ffab8a9743413d09b3d63768e3ec3d6
Joachim Strömbergson <joachim@secworks.se>
2014-05-18 10:21:00 +0200

    Minor fix of instance naming.

--- src/rtl/loop5.v
+++ src/rtl/loop5.v
@@ -17,11 +17,11 @@ module loop5(
   wire d03;
   wire d04;
   
-  inv1 i0(.s(s00), .d(d00));
-  inv1 i1(.s(s01), .d(d01));
-  inv1 i2(.s(s02), .d(d02));
-  inv1 i3(.s(s03), .d(d03));
-  inv1 i4(.s(s04), .d(d04));
+  inv1 i00(.s(s00), .d(d00));
+  inv1 i01(.s(s01), .d(d01));
+  inv1 i02(.s(s02), .d(d02));
+  inv1 i03(.s(s03), .d(d03));
+  inv1 i04(.s(s04), .d(d04));
 
   assign s00 = ctrl ? seed : d04;
   assign s01 = ctrl ? seed : d00;
